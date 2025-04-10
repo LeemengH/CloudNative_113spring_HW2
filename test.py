b841b472987cac4d95719140cab9ef08909d0278
Bill0424 <57516119+Bill0424@users.noreply.github.com>
2025-04-10 18:06:07 +0800

    update test.py

--- test.py
+++ test.py
@@ -4,12 +4,6 @@ if __name__ == '__main__':
 	if sys.argv[1] == "case1":
 		print("case1 execute correctly!")
 	elif sys.argv[1] == "case2":
-<<<<<<< HEAD
 		raise Exception("Wrong execution....")
 	else:
 		raise Exception("Unknown case....")
-=======
-		print("case2 execute correctly!")
-	else:
-		raise Exception("Unknown case....")
->>>>>>> main
