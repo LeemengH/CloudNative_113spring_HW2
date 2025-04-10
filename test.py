47a46ad83a3479bcf71db847a1b8d7a41eb4a41c
Bill0424 <57516119+Bill0424@users.noreply.github.com>
2025-04-10 17:26:53 +0800

    add test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import sys
 
 if __name__ == '__main__':
-	if sys.argv[0] == "case1":
+	if sys.argv[1] == "case1":
 		print("case1 execute correctly!")
-	elif sys.argv[0] == "case2":
+	elif sys.argv[1] == "case2":
 		print("case2 execute correctly!")
 	else:
-		print("Unknown case....")
+		raise Exception("Unknown case....")
